06825b0066fe53a7ec247e7dcdd750a13b482e1f
taynara <taynarafsilvaa@gmail.com>
2022-10-09 15:40:12 -0300

    criacao do projeto

---
+++ README.md
@@ -0,0 +1,4 @@
+## Exercicio de estrutura sequencial
+
+Atividade2:Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("informe o numero: ");
+       int numero = sc.nextInt();
+       sc.close();
+       System.out.println("O numero digitado foi: " + numero);
+
+    
+    }
+    
+
+    }
+
